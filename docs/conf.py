fec0258ebe72222606e083d3e1c5f543960a2613
Zachary Tipnis <tipniz@rpi.edu>
2019-06-05 10:20:13 -0400

    sphinx-to-jekyll

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ "breathe", "exhale", "m2r", "guzzle_sphinx_theme", "sphinxtogithub" ]
+extensions = [ "breathe", "exhale", "m2r", "guzzle_sphinx_theme", 'sphinx_jekyll_builder' ]
 
 exhale_args = {
     # These arguments are required
@@ -63,7 +63,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'build/*']
 
 
 # -- Options for HTML output -------------------------------------------------
